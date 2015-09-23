be17207797705a2def9397956be07814628b1e39
Chris Jadoo <cj12ih@student.ocadu.ca>
2015-09-22 20:04:56 -0400

    Comments added to WallBuilder. Unnecessary scripts deleted

--- Scripts/WallBuilder.cs
+++ Scripts/WallBuilder.cs
@@ -2,24 +2,16 @@
 using System.Collections;
 using System;
 
+/// <summary>
+/// Class that builds the walls and holds the array
+/// that contains all of the tiles
+/// </summary>
 public class WallBuilder : Singleton<WallBuilder> {
-
-	GameObject tile;
-	float tileLen;
-	int xAmt, yAmt, zAmt; //amount of tiles along each axis
-	public delegate void newWallNotifier ();
-	public event newWallNotifier OnNewWall;
-
-	void notifyNewWall() {
-
-		if (OnNewWall != null)
-			OnNewWall();
-	}
 
 	void OnEnable () {
 
 		InitVariables ();
-		BuildsWalls ();
+		BuildWalls ();
 	}
 
 	void InitVariables () {
@@ -27,18 +19,24 @@ public class WallBuilder : Singleton<WallBuilder> {
 		xAmt = WallManager.instance.xAmt;
 		yAmt = WallManager.instance.yAmt;
 		zAmt = WallManager.instance.zAmt;
-		tile = WallManager.instance.tile;
 		tileLen = WallManager.tileDimensions.x;
 	}
-
+	
+	// Array of 6 2D arrays representing walls of tiles
+	// indexs 4 and 5 represent the ceiling and floor respectively
 	[HideInInspector] public Tile[][,] walls;
-	void BuildsWalls () {
+	/// <summary>
+	/// Builds the walls and tiles and fills the walls array
+	/// </summary>
+	void BuildWalls () {
 
-		walls = createWalls ();
+		walls = CreateWalls ();
 		CreateCeilingAndFloor (out walls[4], out walls[5]);
 	}
 
-	Tile[][,] createWalls() {
+	private float tileLen;
+	private int xAmt, yAmt, zAmt;
+	Tile[][,] CreateWalls() {
 
 		Tile[][,] walls = new Tile[6][,];
 		Quaternion locRot = Quaternion.Euler (-90, 0, 0); //face forward then rotate
@@ -66,6 +64,16 @@ public class WallBuilder : Singleton<WallBuilder> {
 		floor = CreateWall(worldRot, locRot, xAmt, zAmt, -yAmt * tileLen /2f, 5);
 	}
 
+	/// <summary>
+	/// Creates a wall of tiles.
+	/// </summary>
+	/// <returns>A 2D array of tiles.</returns>
+	/// <param name="worldRotation">Rotation of the wall.</param>
+	/// <param name="localRotation">Local rotation of each tile.</param>
+	/// <param name="width">Width of the wall.</param>
+	/// <param name="height">Height of the wall.</param>
+	/// <param name="depthShift">Depth shift.</param>
+	/// <param name="wallIndex">Wall index in walls array.</param>
 	Tile[,] CreateWall (Quaternion worldRotation, Quaternion localRotation, int width, int height, float depthShift, int wallIndex) {//return wall array
 
 		Tile[,] wall = new Tile[width, height];
@@ -77,7 +85,7 @@ public class WallBuilder : Singleton<WallBuilder> {
 		Vector3 position = new Vector3 (x, y, z);
 		for (int h = 0; h < height; h++) {
 			for (int w = 0; w < width; w++ ) {
-				GameObject obj = GameObject.Instantiate (tile, worldRotation * position , localRotation) as GameObject;
+				GameObject obj = GameObject.Instantiate (WallManager.instance.tile, worldRotation * position , localRotation) as GameObject;
 				Tile tileInstance = obj.GetComponent<Tile>();
 				tileInstance.wallIndex = wallIndex;
 				tileInstance.wallPosition.x = w;
