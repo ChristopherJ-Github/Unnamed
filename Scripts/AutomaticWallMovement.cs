9a1ea69f5c031cbab15d0169adddf7261e1e9823
CHristopher Jadoo <artboy77@hotmail.com>
2015-10-07 13:26:24 -0400

    AutomaticWallMovement added

---
+++ Scripts/AutomaticWallMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutomaticWallMovement : Singleton<AutomaticWallMovement> {
+	
+	void Start () {
+	
+		StartCoroutine (CountDown ());
+	}
+
+
+	IEnumerator CountDown () {
+
+		float timer = 0.3f;
+		while (timer > 0) {
+			timer -= Time.deltaTime;
+			yield return null;
+		}
+		StartCoroutine (CountDown ());
+		MoveRandomTile ();
+	}
+
+	void MoveRandomTile () {
+
+		int wallIndex = Random.Range(0, 6);
+		int indexX = Random.Range(0, WallManager.instance.xAmt);
+		int indexY = Random.Range(0, WallManager.instance.yAmt);
+		Tile tile = WallManager.instance.walls [wallIndex][indexX, indexY];
+		tile.Move ();
+	}
+
+}
